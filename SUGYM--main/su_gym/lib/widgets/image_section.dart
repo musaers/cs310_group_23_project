f6f590b52a056302f2e8a17c6964a8bcb0ff1dd5
zeynepdgc <152545971+zeynepdgc@users.noreply.github.com>
2025-04-19 17:07:18 +0300

    image_section.dart

--- SUGYM--main/su_gym/lib/widgets/image_section.dart
+++ SUGYM--main/su_gym/lib/widgets/image_section.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+
+
+
 class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
+  const ImageSection({Key? key, required this.image}) : super(key: key);
 
   final String image;
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      image,
-      width: double.infinity, // Adjusts to the full width of its parent
-      height: 720, // Set the height as needed
-      fit: BoxFit.cover,
+    return SizedBox.expand(
+      child: Image.asset(
+        image,
+        fit: BoxFit.cover,
+         color: Colors.black.withOpacity(0.4),
+        colorBlendMode: BlendMode.darken,
+      ),
     );
   }
 }
